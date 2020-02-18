4866ab0fa08a9ccea7b2792bdded769ee786f0ce
Yoki Renaldo Sinaga <yokirenaldo42@gmail.com>
2020-02-18 21:07:04 +0700

    menampilkan data

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,8 +1,6 @@
-//app/config/db.config.js
 module.exports = {
-    HOST: "us-cdbr-iron-east-04.cleardb.net",
-    USER: "bd130a55b5567b",
-    PASSWORD: "9b2fc1a5",
-    DATABASE: "heroku_de0fa41b6e800f7"
-  };
-  
+ HOST: "xxx",
+ USER: "xxx",
+ PASSWORD: "xxx",
+ DATABASE: "xxx"
+};
